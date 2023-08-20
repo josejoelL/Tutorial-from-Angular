570ff45d16f58f988893588074f4257607c97e02
JoJo <18170365@itculiacan.edu.mx>
2023-08-19 23:07:54 -0700

    adding the part 3
    Signed-off-by: JoJo <18170365@itculiacan.edu.mx>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  template: `
+imports: [
+  CommonModule,
+  HousingLocationComponent
+],
+ template: `
   <section>
     <form>
       <input type="text" placeholder="Filter by city">
       <button class="primary" type="button">Search</button>
     </form>
   </section>
-`,
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
+  `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
