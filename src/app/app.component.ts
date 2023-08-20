3575297ed44223bb770a49078f4d378a35fbba64
JoJo <18170365@itculiacan.edu.mx>
2023-08-19 22:30:18 -0700

    Add new practice
    Signed-off-by: JoJo <18170365@itculiacan.edu.mx>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [
+  HomeComponent,
+],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'homes';
 }
