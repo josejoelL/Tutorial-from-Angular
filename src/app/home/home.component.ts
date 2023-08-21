e143fa17538ce32cd89bdc9b994531a042930afb
JoJo <18170365@itculiacan.edu.mx>
2023-08-20 22:53:17 -0700

    Leccion 4 y 5
    Signed-off-by: JoJo <18170365@itculiacan.edu.mx>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -24,5 +24,16 @@ imports: [
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
