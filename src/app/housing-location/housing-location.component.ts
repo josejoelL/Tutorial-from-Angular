4107b4e17ab87c34ab0f6bf9505dd7b31a120946
JoJo <18170365@itculiacan.edu.mx>
2023-08-22 16:31:57 -0700

    Leccion 8 complete
    Signed-off-by: JoJo <18170365@itculiacan.edu.mx>

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,10 +6,12 @@ import { HousingLocation } from '../housinglocation';
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      housing-location works!
-    </p>
+ template: `
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
